96f90ab630f68be3c32741a61e409de26e9e4ce4
nb <disha07@mail.ru>
2021-05-06 19:33:59 +0300

    Упражнение 1. Использование параметров в методах, возвращающих значения

---
+++ Lab4.1/Utils.cs
@@ -0,0 +1,19 @@
+﻿namespace Utils
+{
+    using System;
+
+    class Utils
+    {
+        //
+        // Return the greater of two integer values
+        //
+
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+    }
+}
