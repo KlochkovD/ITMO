35c40720678fafe439a217aad5d34ca194f2e6ad
nb <disha07@mail.ru>
2021-05-06 19:51:17 +0300

    Упражнение 2. Использование в методах параметров, передаваемых по ссылке

---
+++ Lab4.2/Test.cs
@@ -0,0 +1,23 @@
+﻿namespace Utils
+{
+	using System;
+
+	public class Test
+	{
+		public static void Main()
+		{
+			int x;
+			int y;
+
+			Console.WriteLine("Enter first number:");
+			x = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter second number:");
+			y = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Before swap: " + x + "," + y);
+			Utils.Swap(ref x, ref y);
+			Console.WriteLine("After swap: " + x + "," + y);
+		}
+	}
+}
+
---
+++ Lab4.2/Utils.cs
@@ -0,0 +1,21 @@
+﻿namespace Utils
+{
+    using System;
+
+    public class Utils
+    {
+        // As before....
+
+        //
+        // Exchange two integers, passed by reference
+        //
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
+
