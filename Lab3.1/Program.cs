7cedaf304c873e5aff6a7c0166d2ac8cf7015045
nb <disha07@mail.ru>
2021-05-06 20:37:13 +0300

    Упражнение 1. Преобразование дня года в дату типа  месяц - день

--- Lab3.1/Program.cs
+++ Lab3.1/Program.cs
@@ -18,6 +18,7 @@ class WhatDay
 {
     static void Main()
     {
+
         Console.Write("Please enter a day number between 1 and 365: ");
         string line = Console.ReadLine();
         int dayNum = int.Parse(line);
