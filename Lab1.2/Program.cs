6273e197f526225bf2b61bc212dadc43ab342a77
nb <disha07@mail.ru>
2021-05-06 16:09:20 +0300

    class DivideIt

---
+++ Lab1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._2
+{
+    class DivideIt
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first integer");
+            string temp = Console.ReadLine();
+            int i = Int32.Parse(temp);
+
+            Console.WriteLine("Please enter the second integer");
+            temp = Console.ReadLine();
+            int j = Int32.Parse(temp);
+
+        }
+    }
+}
