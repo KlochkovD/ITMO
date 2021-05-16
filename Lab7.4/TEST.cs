c2e794147964a6bdcf3aaf666200099669c88777
nb <disha07@mail.ru>
2021-05-16 17:36:29 +0300

    Упражнение 4. Проверка наличия реализации интерфейса

---
+++ Lab7.4/TEST.cs
@@ -0,0 +1,19 @@
+﻿namespace Utils
+{
+    using System;
+
+    public class Test
+    {
+        public static void Main()
+        {
+            int i = 0;
+            ulong u = 0;
+            string s = "Test";
+
+            Console.WriteLine("int: {0}", Utils.IsItFormattable(i));
+            Console.WriteLine("ulong: {0}", Utils.IsItFormattable(u));
+            Console.WriteLine("String: {0}", Utils.IsItFormattable(s));
+
+        }
+    }
+}
